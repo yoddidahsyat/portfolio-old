c86b7ab11ff847cc502d65d6e7f15c5b2264c722
yoddi <yoddi.dahsyat@gmail.com>
2024-09-07 19:58:21 +0700

    update data work experience upscalix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,46 +1,121 @@
-import React from 'react'
-import { Button, Image } from 'react-bootstrap'
-import profile from '../assets/images/profile-sm.jpg'
+import React from "react";
+import { Button, Image } from "react-bootstrap";
+import profile from "../assets/images/profile-sm.jpg";
 // import Footer from '../components/Footer'
 
 const About = () => {
-    return (
-        <div className='container mt-5'>
-            <div className="row vh-100 align-items-center">
-                <div className='col text-center'>
-                <Image src={profile} alt='profile' width='200' height='200' roundedCircle style={{objectFit: 'cover'}} />
-                <h1 className='my-3'>About me</h1>
-                <p>I am 24 years old, currently live in Purbalingga, Central Java, Indonesia. I am a full stack web developer with great problem solving skills and a good sense of design. I have more than 1 year of experience working as a fullstack and frontend developer.</p>
-                <Button variant='dark' href='/CV%20-%20Mohammad%20Yoddi%20Dahsyat.pdf' target='_blank' >Download Resume</Button>
-                </div>
+  return (
+    <div className='container mt-5 py-5'>
+      <div className='row align-items-center mb-4'>
+        <div className='col text-center'>
+          <Image
+            src={profile}
+            alt='profile'
+            width='200'
+            height='200'
+            roundedCircle
+            style={{ objectFit: "cover" }}
+          />
+          <h1 className='my-3'>About me</h1>
+          <p>I am a Full stack Developer based in Semarang, Indonesia.</p>
+          <Button
+            variant='dark'
+            href='/CV%20-%20Mohammad%20Yoddi%20Dahsyat.pdf'
+            target='_blank'
+          >
+            Download Resume
+          </Button>
+        </div>
+      </div>
+      <div className='row py-4'>
+        <div className='col-md'>
+          <h3>Skills</h3>
+        </div>
+        <div className='col'>
+          <p className='fw-bold'>Coding Languages:</p>
+          <ul>
+            <li>HTML</li>
+            <li>CSS / SCSS</li>
+            <li>Javascript</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>PostgreSQL</li>
+          </ul>
+        </div>
+        <div className='col-md'>
+          <p className='fw-bold'>Frameworks/Libraries:</p>
+          <ul>
+            <li>ReactJs</li>
+            <li>NodeJs</li>
+            <li>ExpressJs</li>
+            <li>jQuery</li>
+            <li>AJAX</li>
+            <li>CodeIgniter</li>
+            <li>Bootstrap</li>
+            <li>React Native</li>
+          </ul>
+        </div>
+        <div className='col'>
+          <p className='fw-bold'>Others:</p>
+          <ul>
+            <li>Git</li>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
+            <li>Figma</li>
+          </ul>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md'>
+          <h3>Work Experience</h3>
+        </div>
+        <div className='d-grid gap-4'>
+          <div className='g-col-12'>
+            <div className='fw-bold'>Upscalix (current)</div>
+            <div>Fullstack Developer</div>
+            <div className='fst-italic'>Dec 2022 - Now</div>
+            <li>
+              Develop, create, and fix web applications for several clients and
+              projects.
+            </li>
+          </div>
+          <div className='g-col-12'>
+            <div className='fw-bold'>uHoo Pte., Ltd.</div>
+            <div>Frontend Developer</div>
+            <div className='fst-italic'>Oct 2021 - Aug 2022</div>
+            <li>
+              Maintain frontend web development for premium and partner
+              dashboard to show visualized data using ReactJs.
+            </li>
+          </div>
+          <div className='g-col-12'>
+            <div className='fw-bold'>PT. Talenta Indonesia Raya</div>
+            <div>Fullstack developer</div>
+            <div className='fst-italic'>Feb - Aug 2021</div>
+            <li>
+              Develop and maintain online psycho-test website application and
+              server to be used by thousands of users concurrently using PHP,
+              mySQL, jQuery, and Google Cloud Platform.
+            </li>
+            <li>
+              Create new assessment kits and modules to be used by clients.
+            </li>
+          </div>
+          <div className='g-col-12'>
+            <div className='fw-bold'>
+              The Ministry of Communication and Information of Indonesia
+              (Kemkominfo)
             </div>
-            <div className="row py-4">
-                <div className="col-md">
-                <h3>Work Experience</h3>
-                </div>
-                <div className="col">
-                <div className="row">
-                    <div className='fw-bold'>uHoo Pte., Ltd.</div>
-                    <div>Web developer (frontend)</div>
-                    <div className='fst-italic'>Oct 2021 - Aug 2022</div>
-                    <li>Maintain frontend web development for premium and partner dashboard to show visualized data using ReactJs.</li>
-                </div>
-                <div className="row mt-4">
-                    <div className='fw-bold'>PT. Talenta Indonesia Raya</div>
-                    <div>Full stack developer</div>
-                    <div className='fst-italic'>Feb - Aug 2021</div>
-                    <li>Develop and maintain online psycho-test website application and server to be used by thousands of users concurrently using PHP, mySQL, jQuery, and Google Cloud Platform.</li>
-                    <li>Create new assessment kits and modules to be used by clients.</li>
-                </div>
-                <div className="row mt-4">
-                    <div className='fw-bold'>The Ministry of Communication and Information of Indonesia (Kemkominfo)</div>
-                    <div>Internship</div>
-                    <div className='fst-italic'>Mar - May 2020</div>
-                    <li>Create a web app for employee management system using PHP and mySQL</li>
-                </div>
-                </div>
-            </div>
-            <div className="row py-4">
+            <div>Internship</div>
+            <div className='fst-italic'>Mar - May 2020</div>
+            <li>
+              Create a web app for employee management system using PHP and
+              mySQL
+            </li>
+          </div>
+        </div>
+      </div>
+      {/* <div className="row py-4">
                 <div className="row">
                 <div className="col-md-2">
                     <h3>Education</h3>
@@ -97,47 +172,9 @@ const About = () => {
                     </div>
                 </div>
                 </div>
-            </div>
-            <div className="row py-4">
-                <div className="col-md-6">
-                <h3>Skills</h3>
-                </div>
-                <div className="col">
-                <p className='fw-bold'>Coding Languages:</p>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS / SCSS</li>
-                    <li>Javascript</li>
-                    <li>PHP</li>
-                    <li>MySQL</li>
-                    <li>PostgreSQL</li>
-                </ul>
-                </div>
-                <div className="col">
-                <p className='fw-bold'>Frameworks/Libraries:</p>
-                <ul>
-                    <li>ReactJs</li>
-                    <li>NodeJs</li>
-                    <li>ExpressJs</li>
-                    <li>jQuery</li>
-                    <li>AJAX</li>
-                    <li>CodeIgniter</li>
-                    <li>Bootstrap</li>
-                    <li>React Native</li>
-                </ul>
-                </div>
-                <div className="col">
-                <p className='fw-bold'>Others:</p>
-                <ul>
-                    <li>Git</li>
-                    <li>Photoshop</li>
-                    <li>Illustrator</li>
-                    <li>Figma</li>
-                </ul>
-                </div>
-            </div>
-        </div>
-    )
-}
+            </div> */}
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import profile from '../assets/images/profile-lg.JPG'
+import React from "react";
+import { Link } from "react-router-dom";
+import profile from "../assets/images/profile-lg.JPG";
 
 const Home = () => {
   return (
     <div className='container'>
-      <div className="row vh-100 align-items-center justify-content-center">
-        <div className="row align-items-center justify-content-center">
+      <div className='row vh-100 align-items-center justify-content-center'>
+        <div className='row align-items-center justify-content-center'>
           <div className='col-md-5 text-md-end mb-3'>
-            <img src={profile} alt='yoddi dahsyat' className='img-fluid w-50' />
+            <img
+              src={profile}
+              alt='yoddi dahsyat'
+              width={250}
+              height={250}
+              className='rounded-circle'
+              style={{ objectFit: "cover", objectPosition: "0% 40%" }}
+            />
           </div>
           <div className='col-md-5'>
             <h1>Hi!</h1>
             <h3>I'm Mohammad Yoddi Dahsyat,</h3>
             <h5>a Full Stack Developer.</h5>
-            <div className="mt-3">
-              <Link className='link-secondary me-3' to='/about'>About</Link>
-              <Link className='link-secondary me-3' to='/projects'>Projects</Link>
-              <Link className='link-secondary me-3' to='/contact'>Contact</Link>
+            <div className='mt-3'>
+              <Link className='link-secondary me-3' to='/about'>
+                About
+              </Link>
+              <Link className='link-secondary me-3' to='/projects'>
+                Projects
+              </Link>
+              <Link className='link-secondary me-3' to='/contact'>
+                Contact
+              </Link>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
